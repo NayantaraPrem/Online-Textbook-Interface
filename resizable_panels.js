76a2a81ac56346afd6df29a175c3c654c87b2f6f
geetikas34 <geetikas34@gmail.com>
2015-11-15 16:40:34 -0500

    Fixed Insert and Delete functionality of Annotations
    -Currently UI displays properly when you add and delete each annotation
    -The annotation data is not yet saved for processing (there is no form
    where it is captured, etc.)

--- resizable_panels.js
+++ resizable_panels.js
@@ -1,24 +1,22 @@
 $(document).on('click', '.panel-heading span.clickable', function (e) {
     var $this = $(this);
-    if (!$this.hasClass('panel-collapsed')) {
+    if (!$this.hasClass('panel-collapsed') && !$this.hasClass('writable')) {
         $this.parents('.panel').find('.panel-body').slideUp();
         $this.addClass('panel-collapsed');
     } else {
         $this.parents('.panel').find('.panel-body').slideDown();
         $this.removeClass('panel-collapsed');
     }
-    return false;
 });
 $(document).on('click', '.panel div.clickable', function (e) {
     var $this = $(this);
-    if (!$this.hasClass('panel-collapsed')) {
+    if (!$this.hasClass('panel-collapsed') && !$this.hasClass('writable')) {
         $this.parents('.panel').find('.panel-body').slideUp();
         $this.addClass('panel-collapsed');
     } else {
         $this.parents('.panel').find('.panel-body').slideDown();
         $this.removeClass('panel-collapsed');
     }
-    return false;
 });
 
 $(document).ready(function () {
@@ -30,23 +28,19 @@ $(document).on('click', '#add_annotation', function () {
         $('#container').append('\
             <div class="row">\
                 <div class="col-md-12">\
-                    <div class="panel panel-primary">\
-                        <div class="panel-heading clickable">\
+                    <div class="panel panel-primary writable">\
+                        <div class="panel-heading clickable writable">\
                             <h3 class="panel-title">\
-                                <form>\
                                     <input class="annt_title" type="text" placeholder="Enter Title" onkeypress="annt_submit(event)"/>\
-                                </form>\
                             </h3>\
                             <span class="pull-right ">\
-                            </span>\
-                                <button class="btn btn-primary btn-sm"> \
+                                <button class="btn btn-primary btn-sm" onclick="delete_annt(event)"> \
                                     <span class="glyphicon glyphicon-trash"></span> \
                                 </button>\
+                            </span>\
                         </div>\
-                        <div class="panel-body">\
-                            <form>\
+                        <div class="panel-body writable">\
                                 <textarea class="annt_body" type="text" placeholder="Enter Note" onkeypress="annt_submit(event)"/>\
-                            </form>\
                         </div>\
                     </div>\
                 </div>\
@@ -56,7 +50,6 @@ $(document).on('click', '#add_annotation', function () {
 function annt_submit(e) {
     if(e.keyCode == 13) {
         // enter pressed 
-        alert("hey");
         var text_box = document.getElementsByClassName('annt_title');
         var text_area = document.getElementsByClassName('annt_body');
         var arrayLength = text_box.length;
@@ -64,7 +57,8 @@ function annt_submit(e) {
         if (arrayLength != arrayLength2) {
             alert("number of textboxes and areas different!");
         }
-        
+        $('.writable').removeClass('writable');
+
         for (var i = 0; i < arrayLength; i++) {
             if(!text_box[i].hasAttribute('readonly')) {
                 text_box[i].setAttribute('readonly', 'readonly');
@@ -75,9 +69,9 @@ function annt_submit(e) {
     return false;
 }
 
-//function delete_annt(e) {
-//    alert('bunnies');
-   // if($(this)
-   // $(this).parent().parent().remove();
-   // return false;
-//}
+function delete_annt(e) {
+   var parent = e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement;
+   parent.remove();
+   e.stopPropagation();
+   return false;
+}
