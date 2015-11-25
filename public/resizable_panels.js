2d0e5216863be2759474e6b137c8586d3cdf9965
Nayantara Prem <prem.nayantara@gmail.com>
2015-11-25 12:13:25 -0500

    Fix jquery and javascript issues while hosting node app, and store values retrieved from annotation(still buggy)

--- public/resizable_panels.js
+++ public/resizable_panels.js
@@ -19,58 +19,91 @@ $(document).on('click', '.panel div.clickable', function (e) {
     }
 });
 
-$(document).ready(function () {
+$(function () {
     $('.panel-heading span.clickable').click();
     $('.panel div.clickable').click();
 });
 
 $(document).on('click', '#add_annotation', function () {
-        $('#container').append('\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <div class="panel panel-primary writable">\
-                        <div class="panel-heading clickable writable">\
-                            <h3 class="panel-title">\
-                                    <input class="annt_title" type="text" placeholder="Enter Title" onkeypress="annt_submit(event)"/>\
-                            </h3>\
-                            <span class="pull-right ">\
-                                <button class="btn btn-primary btn-sm" onclick="delete_annt(event)"> \
-                                    <span class="glyphicon glyphicon-trash"></span> \
-                                </button>\
-                            </span>\
-                        </div>\
-                        <div class="panel-body writable">\
-                                <textarea class="annt_body" type="text" placeholder="Enter Note"/>\
-                        </div>\
-                    </div>\
-                </div>\
-            </div>');
+
+	var new_annot = '\
+		<div class="row">\
+			<div class="col-md-12">\
+				<div class="panel panel-primary writable">\
+					<form name="ajaxform" id="ajaxform" class="form">\
+						<div class="panel-heading clickable writable">\
+							<h3 class="panel-title">\
+								<input class="annt_title" name="title" type="text" placeholder="Enter Title"/>\
+							</h3>\
+							<span class="pull-right ">\
+								<button class="btn btn-primary btn-sm" onclick="delete_annt(event)"> \
+									<span class="glyphicon glyphicon-trash"></span> \
+								</button>\
+							</span>\
+						</div>\
+						<div class="panel-body writable">\
+							<textarea class="annt_body" name="body" type="text" placeholder="Enter Note"/>\
+						</div>\
+						<input type="submit" value="Submit" id="btnSend" />\
+					</form>\
+				</div>\
+			</div>\
+		</div>';
+	$('#container').append(new_annot);
 });
+	$(document).on('click', 'input[type="submit"]', function(e) {
+		e.preventDefault();
+		var inputID = $(this).attr('id');
+		alert(inputID); 
+	//$('#ajaxform').submit(function(e){
+		alert("Clicked");
+		
+		var data = {};
+		data.title = "title";
+		data.message = "message";
+		var postData = $(this).serializeArray();
+		var formURL = $(this).attr("action");
+		$.ajax({
+			type: 'POST',
+			data: JSON.stringify(postData),
+			contentType: 'application/json',
+			url: 'http://localhost:80/ajax',						
+			success: function(data) {
+				//alert('success');
+				//alert(JSON.stringify(postData));
+			},
+			error: function(data){
+				window.console.log(data);
+			}
+		});
+		
+ 		//make text boxes read-only
+		//if(e.keyCode == 13) {
+			//alert("Enter pressed");
+			// enter pressed 
+			var text_box = document.getElementsByClassName('annt_title');
+			var text_area = document.getElementsByClassName('annt_body');
+			var arrayLength = text_box.length;
+			var arrayLength2 = text_area.length;
+			if (arrayLength != arrayLength2) {
+				alert("number of textboxes and areas different!");
+			}
+			$('.writable').removeClass('writable');
 
-function annt_submit(e) {
-    if(e.keyCode == 13) {
-        // enter pressed 
-        var text_box = document.getElementsByClassName('annt_title');
-        var text_area = document.getElementsByClassName('annt_body');
-        var arrayLength = text_box.length;
-        var arrayLength2 = text_area.length;
-        if (arrayLength != arrayLength2) {
-            alert("number of textboxes and areas different!");
-        }
-        $('.writable').removeClass('writable');
+			for (var i = 0; i < arrayLength; i++) {
+				if(!text_box[i].hasAttribute('readonly')) {
+					text_box[i].setAttribute('readonly', 'readonly');
+					text_area[i].setAttribute('readonly', 'readonly');
+				}
+			}
+		//}
 
-        for (var i = 0; i < arrayLength; i++) {
-            if(!text_box[i].hasAttribute('readonly')) {
-                text_box[i].setAttribute('readonly', 'readonly');
-                text_area[i].setAttribute('readonly', 'readonly');
-            }
-        }
-    }
-    return false;
-}
+	return false;
+	});
 
 function delete_annt(e) {
-   var parent = e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement;
+   alert("Deleting");
+   var parent = e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
    parent.remove();
    e.stopPropagation();
    return false;
--- server.js
+++ server.js
@@ -16,10 +16,32 @@ app.use(bodyParser.json());
 // is the route that express uses when we visit
 // our site initially.
 app.get('/', function(req, res){
+  // The form's action is '/' and its method is 'POST',
+  // so the `app.post('/', ...` route will receive the
+  // result of our form on the html page
  
   res.sendFile( __dirname + "/" + "annotation_panels.html" );
 
 });
 
+// This route receives the posted form.
+// As explained above, usage of 'body-parser' means
+// that `req.body` will be filled in with the form elements
+app.post('/ajax', function(req, res){
+ /* var title = req.body.title;
+  var body = req.body.note_body;
+  console.log("req received");*/
+  //console.log("req received:\ntitle: " + title +"\nbody: "+body);
+  //console.log(req.body);
+
+	var obj = {};
+	console.log('Received:');
+//	console.log('annotation title: ' + req.body[0].value);
+//	console.log('annotation contents: ' + req.body[1].value);
+	console.log(JSON.stringify(req.body));
+	console.log('------------------------------------------');
+
+	res.send(req.body);
+});
 
 app.listen(80);
