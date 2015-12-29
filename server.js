618973a7441bde76caca084f8b4b0c9c7195d527
saminahAmin <saminah.amin@gmail.com>
2015-12-28 20:43:28 -0500

    Saminah: Added code for dynamic book display.

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var bodyParser = require('body-parser');
 var converter = require('epub2html');
 var multer = require('multer');
 
-var epubfile = 'testbook.epub'
+var replace = require("replace");
+var path = require('path');
+
+
 // create our app
 var app = express();
 app.use(express.static('public'));
@@ -15,6 +18,40 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
+
+
+function replace_url(from, to) {
+  //console.log(word);
+
+	console.log("Going to open file!");
+		fs.open('book.html', 'r+', function(err, fd) {
+		   if (err) {
+		       return console.error(err);
+		   }
+		  console.log("File opened successfully!");     
+		
+
+  		replace({
+			    regex: from ,
+			    replacement: to,
+			    paths: ['book.html'],
+			    recursive: true,
+			    silent: true,
+			});
+		 console.log('Replace successful!');
+
+
+		fs.close(fd, function(err){
+         if (err){
+            console.log(err);
+         } 
+         console.log("File closed successfully.");
+        });
+
+	});
+}
+
+
 
 
 var uploading = multer({ dest: __dirname + '/public/uploads/',
@@ -29,6 +66,7 @@ var uploading = multer({ dest: __dirname + '/public/uploads/',
         done=true;
     }
 });
+
 
 // A browser's default method is 'GET', so this
 // is the route that express uses when we visit
@@ -46,44 +84,235 @@ app.get('/annotations', function(req, res){
 });
 
 
-//URL to localhost/book is called onclicking the 'get book' button in annotation_panels.html
+//THIS IS THE HOMEPAGE
 app.get('/', function(req, res){
+
+  res.sendFile( __dirname + "/" + "Home.html");
+  //home.html has an on-click event that will invoke app.get/book<number> below, to redirect to appropriate table of contents.
+
+});
+
+
+//URLs to localhost/book<number> is called onclicking the 'get book' button on Home.html
+//Given a list of available books, get the chosen number from user
+//Based on input, do path-replace logic to decide the correct parent folder path (Book1 or Book2 etc) to append to the href paths in output TOC (book.html). 
+//Ensure the parent folder contains the extracted epub and especially the META-INF/conatiner.xml. (Extraction is scripted but can be manual as well)
+
+app.get('/book1', function(req, res){
+
+
+		
+		var epubfile = 'Public/Books/Book1/testbook.epub';
+
+		var check_xml = 'Public/Books/Book1/META-INF/container.xml';
+
+
+		fs.stat(check_xml, function(err, stat){
+
+			   
+
+		        if (err) { 
+			   
+				var extract = require('extract-zip');
+				var tar = 'Public/Books/Book1/';
+				extract(epubfile, {dir: tar}, function (err) {
+						
+						if(err){
+							console.log('Error Extracting');
+						}
+
+						else 
+							console.log('Extracting');
+				 // extraction is complete. make sure to handle the err 
+					});
+				  }
+
+			   else {
+       				console.log('Already Extracted');
+		        } 
+
+		});
+		 
+
+		converter.parse(epubfile, function (err, epubData) {
+		
+		var htmlData = converter.convertMetadata(epubData);
+		//console.log(htmlData); //Debugging
+
+		//THIS METHOD DOESN'T WORK:
+		//document.write(htmlData);
+
+		//USE FILE WRITE INSTEAD
+		fs.writeFile('book.html', htmlData.htmlNav, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlNav successfully sent to book.html!');
+		});
+
+		/*
+		fs.appendFile('book.html', htmlData.htmlMetas, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlMetas successfully sent to book.html!');
+		});
+
+		fs.appendFile('book.html', htmlData.htmlMetaList, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlMetaList successfully sent to book.html!');
+		});*/
+
+
+		var find = "href=\"" ;
+		var rep = "href=\"Books/Book1/";
+		replace_url(find, rep);
+
+		});
+
+
+  res.sendFile( __dirname + "/" + "book.html" );
+
+
+});
+
+app.get('/book2', function(req, res){
+
+		var epubfile = 'Public/Books/Book2/Relativity.epub';
+
+		var check_xml = 'Public/Books/Book2/META-INF/container.xml';
+
+
+		fs.stat(check_xml, function(err, stat){
+
+			   
+
+		        if (err) { 
+			    
+				var extract = require('extract-zip');
+				var tar = 'Public/Books/Book2/';
+				extract(epubfile, {dir: tar}, function (err) {
+						
+						if(err){
+							console.log('Error Extracting');
+						}
+						else
+							console.log('Extracting');
+				 	});
+				  }
+
+			   else {
+       				console.log('Already Extracted');
+		        } 
+
+		});
+		
 
 
 	  converter.parse(epubfile, function (err, epubData) {
 		
+
 		var htmlData = converter.convertMetadata(epubData);
-		//console.log(htmlData);
+		//console.log(htmlData); //Debugging
 
-		//THESE METHODS DON'T WORK:
+		//THIS METHOD DOESN'T WORK:
 		//document.write(htmlData);
-		//fs.writeFile('book.html', htmlData, function (err) {
-	 	// if (err) return console.log(err);
-	 	// console.log('book output error!');
-		//});
 
-	    //ALTERNATIVE:
-	  	//Sending output to client in html file is not recommended- could not find examples to write htmlData object VALUE to an html file 
-	  	//Can send as JSON file instead using stringify, but need to know what to do with the JSON on the client side
-	  	//Where is the client side receiving the output? Should it do a jquery get?
-			var outputFilename = 'my.json';
-
-			fs.writeFile(outputFilename, JSON.stringify(htmlData, null, 4), function(err) {
-	    	if(err) {
-	     	 //console.log(err);
-	    	} else {
-	      	//console.log("JSON saved to " + outputFilename);
-	    	}
-		 }); 
+		//USE FILE WRITE INSTEAD
+		fs.writeFile('book.html', htmlData.htmlNav, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlNav successfully sent to book.html!');
 		});
 
-	//output.html is a static file at the moment.
-	//It was generated manually beforehand and moved to the public directory
-	//generated by running the test.js in epub2html npm, on the console and sending the console output to an html file
-  res.sendFile( __dirname + "/" + "output.html" );
+		/*fs.appendFile('book.html', htmlData.htmlMetas, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlMetas successfully sent to book.html!');
+		});
+
+		fs.appendFile('book.html', htmlData.htmlMetaList, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlMetaList successfully sent to book.html!');
+		}); */
+
+
+			var find = "href=\"" ;
+			var rep = "href=\"Books/Book2/";
+			replace_url(find, rep);
+		});
+
+
+  res.sendFile( __dirname + "/" + "book.html" );
 
 
 });
+
+
+
+app.get('/book3', function(req, res){
+
+		var epubfile = 'Public/Books/Book3/SCMP.epub';
+		var check_xml = 'Public/Books/Book3/META-INF/container.xml';
+
+
+		fs.stat(check_xml, function(err, stat){
+
+			   
+
+		        if (err) { 
+			    
+				var extract = require('extract-zip');
+				var tar = 'Public/Books/Book3/';
+				extract(epubfile, {dir: tar}, function (err) {
+						
+						if(err){
+							console.log('Error Extracting');
+						}
+						else
+							console.log('Extracting');
+				 	});
+				  }
+
+			   else {
+       				console.log('Already Extracted');
+		        } 
+
+		});
+		
+
+
+	  converter.parse(epubfile, function (err, epubData) {
+		
+
+		var htmlData = converter.convertMetadata(epubData);
+		//console.log(htmlData); //Debugging
+
+		//THIS METHOD DOESN'T WORK:
+		//document.write(htmlData);
+
+		//USE FILE WRITE INSTEAD
+		fs.writeFile('book.html', htmlData.htmlNav, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlNav successfully sent to book.html!');
+		});
+
+		/*fs.appendFile('book.html', htmlData.htmlMetas, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlMetas successfully sent to book.html!');
+		});
+
+		fs.appendFile('book.html', htmlData.htmlMetaList, function (err) {
+	 	 if (err) return console.log(err);
+	 	 console.log('htmlMetaList successfully sent to book.html!');
+		}); */
+
+			var find = "href=\"" ;
+			var rep = "href=\"Books/Book3/";
+			replace_url(find, rep);
+		});
+
+
+  res.sendFile( __dirname + "/" + "book.html" );
+
+
+});
+
+
 
 app.get('/upload_img', function(req, res){
 	res.sendFile( __dirname + "/" + "upload_img.html" );
