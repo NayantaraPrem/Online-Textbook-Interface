ebc52a2d97cbd1b8a210d42ffe08e962686deb4d
Fateha K <fateha.khan@utoronto.ca>
2016-01-07 05:16:46 -0500

    add combined annotations and sample textbook display --temp hardcoded textbook

---
+++ public/template.js
@@ -0,0 +1,61 @@
+	document.write(' \
+	<div>	\
+		<ul class="page_nav_buttons"> \
+			<li class="page_nav"><a class="active" href="title.html">The War of the Worlds</a></li> \
+			<li class="page_nav"><a href="localhost:80/home">Bookshelf</a></li> \
+			<li class="page_nav"><a href="localhost:80/settings">Settings</a></li> \
+		</ul> \
+	</div> \
+	<div id="nav"> \
+		<ul class="book_nav_buttons">  \
+			<!-- <li class="book_nav"><a href="title.html">Title</a></li> --> \
+			<li class="book_nav"><a href="about.html">About</a></li> \
+			<li class="book_nav"><a href="main1.html">Part 1 - The Coming of the Martians</a> \
+				<ul class="book_nav_buttons"> \
+					<li class="book_nav"><a href="main2.html">Chapter 1 - The Eve of the War</a></li>  \
+					<li class="book_nav"><a href="main3.html">Chapter 2 - The Falling Star</a></li>  \
+					<li class="book_nav"><a href="main4.html">Chapter 3 - On Horsell Common</a></li>  \
+					<li class="book_nav"><a href="main5.html">Chapter 4 - The Cylinder Opens</a></li>  \
+					<li class="book_nav"><a href="main6.html">Chapter 5 - The Heat Ray</a></li>  \
+					<li class="book_nav"><a href="main7.html">Chapter 6 - The Heat-Ray in the Chobham Road</a></li>  \
+					<li class="book_nav"><a href="main8.html">Chapter 7 - How I Reached Home</a></li>  \
+					<li class="book_nav"><a href="main9.html">Chapter 8 - Friday Night</a></li>  \
+					<li class="book_nav"><a href="main10.html">Chapter 9 - The Fighting Begins</a></li>  \
+					<li class="book_nav"><a href="main11.html">Chapter 10 - In the Storm</a></li>  \
+					<li class="book_nav"><a href="main12.html">Chapter 11 - At the Window</a></li>  \
+					<li class="book_nav"><a href="main13.html">Chapter 12 - What I Saw of the Destruction of Weybridge and Shepperton</a></li>  \
+					<li class="book_nav"><a href="main14.html">Chapter 13 - How I Fell in with the Curate</a></li>  \
+					<li class="book_nav"><a href="main15.html">Chapter 14 - In London</a></li>  \
+					<li class="book_nav"><a href="main16.html">Chapter 15 - What Had Happened in Surrey</a></li> \
+					<li class="book_nav"><a href="main17.html">Chapter 16 - The Exodus from London</a></li>  \
+					<li class="book_nav"><a href="main18.html">Chapter 17 - The "Thunder Child"</a></li>  \
+				</ul>  \
+			</li>  \
+			<li class="book_nav"><a href="main19.html">Part 2 - The Earth Under the Martians</a> \
+				<ul class="book_nav_buttons"> \
+					<li class="book_nav"><a href="main20.html">Chapter 1 - Under Foot</a></li> \
+					<li class="book_nav"><a href="main21.html">Chapter 2 - What We Saw from the Ruined House</a></li>  \
+					<li class="book_nav"><a href="main22.html">Chapter 3 - The Days of Imprisonment</a></li>  \
+					<li class="book_nav"><a href="main23.html">Chapter 4 - The Death of the Curate</a></li>  \
+					<li class="book_nav"><a href="main24.html">Chapter 5 - The Stillness</a></li>  \
+					<li class="book_nav"><a href="main25.html">Chapter 6 - The Work of Fifteen Days</a></li>  \
+					<li class="book_nav"><a href="main26.html">Chapter 7 - The Man on Putney Hill</a></li>  \
+					<li class="book_nav"><a href="main27.html">Chapter 8 - Dead London</a></li>  \
+					<li class="book_nav"><a href="main28.html">Chapter 9 - Wreckage</a></li>  \
+					<li class="book_nav"><a href="main29.html">Chapter 10 - The Epilogue</a></li>  \
+				</ul>  \
+			</li>  \
+			<li class="book_nav"><a href="similar.html">Recommendations</a></li> \
+		</ul> \
+	</div>  \
+	<div id="aside" class="container-fluid"> \
+	    <h3 class="heading">My Annotations</h3> \
+		<div class="row, col-sm-4"> \
+			<p> \
+				<button id="add_annotation" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-plus-sign"></span> \
+					Add \
+				</button> \
+			</p> \
+		</div> \
+	</div> \
+	');
